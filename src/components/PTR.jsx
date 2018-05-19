fa1dd815bfffa2928eb7cb148620c2878782314b
Casper Engelmann <me@casperengelmann.com>
2018-05-20 01:16:35 +0200

    Moved server names and addresses into their own file

--- src/components/PTR.jsx
+++ src/components/PTR.jsx
@@ -7,57 +7,9 @@ no-plusplus: 0,
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 
+import servers from '../data/servers';
+
 const PTR = ({ address }) => {
-  const servers = [
-    {
-      name: 'ST1',
-      address: 'wsb24.surf-town.net',
-    },
-    {
-      name: 'ST2',
-      address: 'wsb22.surf-town.net',
-    },
-    {
-      name: 'ST3',
-      address: 'wsb1.surf-town.net',
-    },
-    {
-      name: 'ST4',
-      address: 'wsb5.surf-town.net',
-    },
-    {
-      name: 'ST5',
-      address: 'wsb1.surf-town.net',
-    },
-    {
-      name: 'ST6',
-      address: 'wsb5.surf-town.net',
-    },
-    {
-      name: 'ST7',
-      address: 'wsb5.surf-town.net',
-    },
-    {
-      name: 'ST8',
-      address: 'wsb6.surf-town.net',
-    },
-    {
-      name: 'ST9',
-      address: 'wsb19.surf-town.net',
-    },
-    {
-      name: 'ST10',
-      address: 'wsb7.surf-town.net',
-    },
-    {
-      name: 'ST11',
-      address: 'wsb8.surf-town.net',
-    },
-    {
-      name: 'ST12',
-      address: 'wsb21.surf-town.net',
-    },
-  ];
   let counter = 1;
 
   return (
@@ -79,7 +31,10 @@ const PTR = ({ address }) => {
           return (null);
         })
       }
-      <div className="mb-5" />
+      {
+        servers.some(server => server.address === address) &&
+        <div className="mb-5" />
+      }
     </Fragment>
   );
 };
