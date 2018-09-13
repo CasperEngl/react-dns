1d3e39c2a5a9fd1a661d73624c19c2d784a573fb
Casper Engelmann <me@casperengelmann.com>
2018-09-13 15:02:23 +0200

    Removed unused setTimeout

--- src/actions/dnsActions.js
+++ src/actions/dnsActions.js
@@ -6,6 +6,7 @@ no-multi-spaces: 0,
 camelcase: 0,
 quotes: 0,
 func-names: 0,
+no-undef: 0,
 */
 
 export const GET_DNS = 'GET_DNS';
@@ -13,10 +14,9 @@ export const RESET_DNS = 'RESET_DNS';
 
 export function getDNS(query) {
   return async function (dispatch) {
-    setTimeout(async () => {
-      try {
-        const result = await fetch(`https://support.wkt.dk/public/dns.php?domain=${query}`);
-        const dns = await result.json();
+    try {
+      const result = await fetch(`https://support.wkt.dk/public/dns.php?domain=${query}`);
+      const dns = await result.json();
 
       dispatch({
         type: GET_DNS,
